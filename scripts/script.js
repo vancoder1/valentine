5e01411d5c567cd48eee1e2a73e48c928ae9f8f7
vancoder1 <ivanzaporozhets25@gmail.com>
2025-02-02 02:01:42 -0700

    Add basically everything I needed to add

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    const phrases = ['pleasee🥺', 'I beg you', ':c', ':\'(', 'pleaseee Moaa🥺', 'I know you want it..',
+        'pretty pleasee🥺', 'babe pleaseee🥺'
+    ];
+    let multiplier = 1;
+    let font_size = 16;
+    $('#no-btn').click(function() {
+        if (font_size >= 200) {
+            $('#no-btn').css('display', 'none');
+        }
+        font_size += multiplier;
+        multiplier *= 2;
+        $('#yes-btn').css('font-size', `${font_size}px`);
+        $('#no-btn').text(phrases[Math.floor(Math.random() * phrases.length)]);
+    });
+});
